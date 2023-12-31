8be1cae6ad89cbe6ba53e63ba5a32bcb57ce435a
null <null@null>
2023-12-31 17:51:24 +0800

    work out more admin checking stuff

--- admin.py
+++ admin.py
@@ -1,5 +1,5 @@
 import datetime
-from flask import Blueprint, request, make_response
+from flask import Blueprint, request, make_response, redirect
 import view
 from view import ld_page, mk_page, mk_tagbox
 import settings
@@ -13,8 +13,10 @@ def timestamp(x):
 
 def check_login():
     checkpass = request.cookies.get("admin")
+    print(checkpass)
     if checkpass != settings.password:
         return "<meta http-equiv='refresh' content='0;URL=/admin'>"
+    return None
 
 @admin.route('/admin/', methods=["POST", "GET"])
 def login():
@@ -26,21 +28,25 @@ def login():
     
     data = request.form.copy()
     if data["pw"] != settings.password:
-        return "Fail"
+        return "<meta http-equiv='refresh' content='0;URL=/admin'>"
     response = make_response(redirect('/admin/'))
     response.set_cookie("admin", data["pw"])
     return response
 
 @admin.route('/admin/menu/')
 def menu():
-    check_login()
+    if check_login():
+        return check_login()
     return mk_page(ld_page("admin_menu"))
 
 @admin.route('/admin/threads/')
 def threads():
+    if check_login():
+        return check_login()
     with open("threads/index.txt") as index:
         index = index.read().splitlines()
     index = [i.split("<>") for i in index]
+    index.sort(key = lambda x: x[1], reverse=True)
     page = ""
     page += "<ul>"
     for i in index:
