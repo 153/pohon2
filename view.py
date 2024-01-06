086a2734233154b9013c8d55358e2ce2ad8df7c4
null <null@null>
2024-01-07 00:08:07 +0800

    add second line break when comment is reply in >> link

--- view.py
+++ view.py
@@ -320,7 +320,7 @@ def recent_posts():
             if replynum[-2] != "1":
                 parent = replynum[-2]
                 parentnum = ":".join(replynum[:-1])
-                parent = f"<a href='/thread/{p[1]}#{parentnum}'>&gt;&gt;{parent}</a><br>"
+                parent = f"<a href='/thread/{p[1]}#{parentnum}'>&gt;&gt;{parent}</a><br><br>"
                 p[4] = parent + p[4]
             replynum = replynum[-1]
         postnum = f"<a href='/thread/{p[1]}#{p[3]}'>{replynum}.</a>"
